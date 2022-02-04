42cbc1f172e1fd3440cfa8ce6a163c6029648e0b
Pernixy <darkmastery27@gmail.com>
2022-02-03 18:04:39 -0600

    some stuff

--- sketch.js
+++ sketch.js
@@ -1,42 +1,53 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-  }
+  createCanvas(windowWidth, windowHeight);
+  noCursor();
+
+  colorMode(HSB, 360, 100, 100);
+  rectMode(CENTER);
+  noStroke();
+}
+
+var size = 1.5
+var count = 30;
+var index = 0
+var z = 30;
+var shapes = [0 , 1, 2, 3]
+
+function draw() {
+  background(0);
   
-console.log("Hello there");
-speed = 25;
-x = 200;
-  function draw() {
-    background(120, 185, 160);
+  for (var i = 0; i < 60; i++) {
+    for (var j = 0; j < 60; j++) {
+      var x = i * 50 + 2;
+      var y = j * 50 + 2;
+      var d = size;
+      z += .000000001;
 
-    //bottom right square
-    strokeWeight(5);
-    stroke(0);
-    fill(217, 201, 28);
-    rect(windowWidth/2, windowHeight/2, windowHeight);
+      var r = z;
+      fill(255);
+      rotate(r / 2);
+      rect(mouseX, y, d);
 
-    //top left square
-    strokeWeight(5);
-    stroke(0);
-    fill(252, 152, 3);
-    rect(windowWidth/2, windowHeight/2, windowWidth * -1);
-
-    //bottom left square
-    strokeWeight(5);
-    stroke(0);
-    fill(51, 69, 166);
-    rect(0, windowHeight/2, windowWidth/2);
-
-    //top left square
-    strokeWeight(5);
-    stroke(0);
-    fill(93, 201, 62);
-    rect(windowWidth, windowHeight/2, -windowWidth/2);
-
-    x += speed;
-    if (x >= windowWidth - 25) {
-      speed = speed * -1;
-    }
-    if (x <= 0 + 25) {
-      speed = speed * -1;
+      
     }
   }
+
+  fill(50, 201, 152);
+  circle(0, 0, mouseX * 2);
+}
+
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    if (z < 100) {
+      z += 5;
+    }
+  } else if (keyCode == DOWN_ARROW) {
+      z -= 5;
+  }
+
+  function mouseClicked() {
+    count += 10;
+  }
+
+  
+}
